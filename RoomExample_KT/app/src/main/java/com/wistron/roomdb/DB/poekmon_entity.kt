f5eb780c82facf65bc2914c06f13ca9e5c57393c
Alex <alex_yh_huang@wistron.com>
2019-11-13 19:13:10 +0800

    run time permission example

---
+++ RoomExample_KT/app/src/main/java/com/wistron/roomdb/DB/poekmon_entity.kt
@@ -0,0 +1,16 @@
+package com.wistron.roomdb.DB
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "pokemon_table")
+class Pokemon_Eitity
+{
+    @PrimaryKey
+    var id: Int = 0
+
+    var name:String = ""
+
+    var img:
+
+}
---
+++ RoomExample_KT/app/src/main/java/com/wistron/roomdb/UI/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.wistron.roomdb.UI
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.wistron.roomdb.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
